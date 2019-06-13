16e4864932be932e46dcbc618d562f804ebee0c1
Kjoobin <imkj1148@gmail.com>
2019-06-13 10:28:07 +0900

    Fixed modal,

--- static/modal.js
+++ static/modal.js
@@ -40,7 +40,6 @@ function closeModal() {
   var div = document.createElement("div");
   div.classList.add("close","F(r)","Sq(30px)","pos(r)");
   div.addEventListener('click',removeModal)
-  div.innerHTML = `<svg class="Scale(.5) Expand" width="22" height="22" viewBox="0 0 22 22"><path class="Fill($c-divider) close:h_Fill($c-gray)" d="M14.6 11.7v-1.4l6.5 6.5c1.3 1.2 1.3 3 0 4.3-1 1.3-3 1.3-4.2 0l-6.5-6.4h1.4L5.2 21c-1.2 1.3-3 1.3-4.3 0-1.3-1-1.3-3 0-4.2l6.4-6.5v1.4L1 5.2C-.4 4-.4 2.2 1 1 2-.4 4-.4 5 1l6.5 6.4h-1.4L16.8 1C18-.4 19.8-.4 21 1c1.3 1 1.3 3 0 4.2l-6.4 6.5z" stroke="none" stroke-width="1"></path></svg>`
   document.querySelector(".profile div div").appendChild(div);
 }
 
